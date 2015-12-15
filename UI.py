2398a57ddf74f55037e702b253ba3d6b9bdb38d1
nihilistas-suenos <pinchao_malaga@hotmail.com>
2015-12-15 02:20:14 +0100

    Update UI.py

--- UI.py
+++ UI.py
@@ -3,7 +3,7 @@ twitterbot version 1.0
 """
 from logic import *
 from twython import Twython, TwythonError
-
+from sys import argv
 
 """	
 Get info:
@@ -22,21 +22,22 @@ Get info:
 
 def main():
         
-        try:
-                index = ""
-                while 1:
-                        username = ""
-                        if index == "":
-                        	t = set_params()
-                        opcion = options()
-                        if opcion == "1":
-                                wrap_favs(user_db,username,t)
-                        elif opcion == "2":
-                                mssg = 0
-                                while mssg == 0:
-                                	mssg = send_tweet(t)
-                        elif opcion == "0": exit(0)
-                        elif opcion == "-1": index = ""
+        try:		
+        		index = ""
+        		
+    			while 1:
+                        	username = ""
+                        	if index == "":
+                        		t,username = set_params()
+                        	opcion = options()
+                        	if opcion == "1":
+                                	wrap_favs(user_db,username,t)
+                        	elif opcion == "2":
+                                	mssg = 0
+                                	while mssg == 0:
+                                		mssg = send_tweet(t)
+                        	elif opcion == "0": exit(0)
+                        	elif opcion == "-1": index = ""
                                 
         except TwythonError as e: print (e)
 
@@ -45,4 +46,4 @@ def main():
 if __name__ == "__main__":
         main()
 
-        
+  
